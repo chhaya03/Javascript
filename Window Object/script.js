427b821f97c523ffb570c84fb96e44e4877784b1
Chhaya Mishra <141479837+chhaya03@users.noreply.github.com>
2024-03-08 00:10:50 +0530

    Add files via upload

---
+++ Window Object/script.js
@@ -0,0 +1,4 @@
+// Window Object -->  The window object represents an open window in a browser. it is browser's object (Not js's) and is automatically created by browser.
+// It is global object with lots of properties & methods . 
+
+console.log(window);
